a58bdd4a96a6865b42e37c21deb369452a3efbe6
Vaishak Kallore <vaikoovery@gmail.com>
2019-06-30 15:02:48 +0530

    Initial commit and question 1

---
+++ assets/js/js-functions.js
@@ -0,0 +1,17 @@
+/**
+ * Question 1
+ */
+/*
+const arr = [1, 2, 3, 4]
+
+arr.map(foo)
+arr.map(fooTwo)
+
+function foo(item, index) {
+  console.log(`Foo Item: ${index} - ${item}`)
+}
+
+const fooTwo = (item, index) => {
+  console.log(`FooTwo: ${index} - ${item}`)
+}
+ */
